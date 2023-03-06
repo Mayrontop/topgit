26296dc2c56d9f0f637195ba3e1c6a6823f25115
Mayrontop <nikita12ks@gmail.com>
2023-03-06 21:52:21 +0300

    jhh

--- ConsoleApplication8/Header.h
+++ ConsoleApplication8/Header.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <iostream>
+using namespace std;
 
+void function(int a)
+{
+	a = 10;
+	cout << "a";
+}
 
 
 
--- ConsoleApplication8/Source.cpp
+++ ConsoleApplication8/Source.cpp
@@ -11,7 +11,7 @@ int main()
 
 	std::cout << "Hello";
 
-	//function(a);
+	function(a);
 
 
 
