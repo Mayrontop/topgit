7ba47c9440fc052c7fcc1a36230210c1e357ab50
Mayrontop <nikita12ks@gmail.com>
2023-03-06 21:09:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <typeinfo>
+#include <cstdlib>
+#include <random>
+#include <windows.h>
+#include <utility>
+#include<algorithm>
+#include<string>
+#include<stdio.h>
+#include <time.h>       
+#include <chrono>
+#include <thread>
+#include<cstdio>
+
+using namespace  std;
+
+
+
+
+int main()
+{ 
+	setlocale(LC_ALL, "ru");
+
+
+
+
+
+
+
+   
+}
+
